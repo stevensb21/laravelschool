9c6b5de596049986bda71f076bcdaa7f5390ee89
stevensb <agliullin61@gmail.com>
2025-07-26 23:18:51 +0300

    fixmigrateteacher2

--- school/database/migrations/2025_07_20_121537_add_primary_key_to_teachers_table.php
+++ school/database/migrations/2025_07_20_121537_add_primary_key_to_teachers_table.php
@@ -8,17 +8,20 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration {
     public function up()
     {
-        // Проверяем, есть ли уже первичный ключ на колонке id
+        // Проверяем, есть ли уже первичный ключ teachers_pkey
         $hasPrimaryKey = DB::select("
             SELECT COUNT(*) as count 
             FROM information_schema.table_constraints 
             WHERE table_name = 'teachers' 
+            AND constraint_name = 'teachers_pkey'
             AND constraint_type = 'PRIMARY KEY'
         ")[0]->count;
 
         if ($hasPrimaryKey == 0) {
             // Добавляем PRIMARY KEY к id, только если его нет
             DB::statement('ALTER TABLE teachers ADD PRIMARY KEY (id);');
+        } else {
+            echo "Primary key teachers_pkey already exists on teachers table.\n";
         }
     }
 
